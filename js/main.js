053bed857b2f5df86147e1489a0a7a313e6fec97
ali-rashad-ux <134567209+ali-rashad-ux@users.noreply.github.com>
2023-05-25 07:44:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// JavaScript Document
+//window.alert(window.innerHeight+','+window.innerWidth)
+// animation progress par
+let section = document.querySelector(".three");
+let spans = document.querySelectorAll(".progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop-300) {
+    console.log("Reached Section Three");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+
+//animate on scroll
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      mirror: false,
+    });
+  
+ /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+//safty
+
+	  document.addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+      });
+
+     document.addEventListener("keydown", function (event) {
+        if (event.ctrlKey && (event.keyCode === 67 || event.keyCode === 86 || event.keyCode === 85 || event.keyCode === 117)) {
+        event.preventDefault();
+         }
+     });
+
+    document.addEventListener("keydown", function (event) {
+      if(event.keyCode === 123) {
+         event.preventDefault();
+      }
+     });
+	  
+	 document.addEventListener("keydown", function(event){
+		 if(event.shiftKey && (event.ctrlKey && (event.keyCode === 67 || event.keyCode === 73 || event.keyCode === 74))){
+		 
+	 event.preventDefault();
+         }
+     });
+	  
+	  
+document.addEventListener("mousedown", function(event) {
+      if (event.button === 2 || event.ctrlKey) {
+        event.preventDefault();
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      if (event.ctrlKey && (event.keyCode === 44 || event.keyCode === 45)) {
+        event.preventDefault();
+      }
+    });
+
