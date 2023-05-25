2362fc89043bb0ce6f4fa3fc3af34a34bf4ed620
ali-rashad-ux <134567209+ali-rashad-ux@users.noreply.github.com>
2023-05-25 14:28:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let section = document.querySelector(".three");
 let spans = document.querySelectorAll(".progress span");
 
 window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop-300) {
+  if (window.scrollY >= section.offsetTop-400) {
     console.log("Reached Section Three");
     spans.forEach((span) => {
       span.style.width = span.dataset.width;
